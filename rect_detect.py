3fe990211a7dc2b85eaa0c362a9148329c654233
artata <artata@umich.edu>
2020-11-26 12:41:59 -0500

    main cropper

--- rect_detect.py
+++ rect_detect.py
@@ -12,53 +12,6 @@ height, width = img.shape
 # Create a grayscale image and denoise it
 im_gs = cv2.fastNlMeansDenoising(img, h=3)
 
-# # Create an inverted B&W copy using Otsu (automatic) thresholding
-# im_bw = cv2.threshold(im_gs, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-
-# # Detect lines in this image. Parameters here mostly arrived at by trial and error.
-# lines = cv2.HoughLinesP(
-#     im_bw, 1, np.pi / 180, 200, minLineLength=width / 12, maxLineGap=width / 150
-# )
-
-# # Collect the angles of these lines (in radians)
-# angles = []
-# for line in lines:
-#     x1, y1, x2, y2 = line[0]
-#     angles.append(np.arctan2(y2 - y1, x2 - x1))
-
-# # If the majority of our lines are vertical, this is probably a landscape image
-# landscape = np.sum([abs(angle) > np.pi / 4 for angle in angles]) > len(angles) / 2
-
-# # Filter the angles to remove outliers based on max_skew
-# if landscape:
-#     angles = [
-#         angle
-#         for angle in angles
-#         if np.deg2rad(90 - max_skew) < abs(angle) < np.deg2rad(90 + max_skew)
-#     ]
-# else:
-#     angles = [angle for angle in angles if abs(angle) < np.deg2rad(max_skew)]
-
-# if len(angles) < 5:
-#     # Insufficient data to deskew
-#     im.show()
-
-# # Average the angles to a degree offset
-# angle_deg = np.rad2deg(np.median(angles))
-
-# # If this is landscape image, rotate the entire canvas appropriately
-# if landscape:
-#     if angle_deg < 0:
-#         im_gs = cv2.rotate(im_gs, cv2.ROTATE_90_CLOCKWISE)
-#         angle_deg += 90
-#     elif angle_deg > 0:
-#         im_gs = cv2.rotate(im_gs, cv2.ROTATE_90_COUNTERCLOCKWISE)
-#         angle_deg -= 90
-
-# # Rotate the image by the residual offset
-# M = cv2.getRotationMatrix2D((width / 2, height / 2), angle_deg, 1)
-# im_gs = cv2.warpAffine(im_gs, M, (width, height), borderMode=cv2.BORDER_REPLICATE)
-
 
 _, threshold = cv2.threshold(im_gs, 240, 255, cv2.THRESH_BINARY)
 contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)

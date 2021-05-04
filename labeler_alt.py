75deb3e95bd0cb1afd5cb11b22f134059ba8719c
William Chown <chownwil@umich.edu>
2021-05-04 17:15:08 -0400

    A Labeler for an alternate folder structure if the bubbles_2 folder takes too much space

---
+++ labeler_alt.py
@@ -0,0 +1,99 @@
+#USAGE: python3 labeler.py <dirname>
+import sys
+import os
+import csv
+import pandas as pd
+import numpy as np
+import easygui
+import time
+import shutil
+from tqdm import tqdm
+
+mark_dictionary = {'0': 'no mark',
+                   '1': 'properly bubbled',
+                   '2': 'X marked',
+                   '3': 'check marked',
+                   '4': 'bubbled and crossed out',
+                   '5': 'poorly extracted',
+                   '6': 'other mark',
+                   'u': 'undo',
+                   '1u': 'undo',
+                   'p': 'print run info',
+                   's': 'saves progress',
+                   'q': 'saves and quits',
+                   'qw/os': 'quits without saving'}
+
+prompt = '---Menu---\n'
+for i in mark_dictionary:
+    prompt += i + ': ' + mark_dictionary[i] + '\n'
+
+def get_label_folder_path(directory):
+    root = 'Pueblo_labels/'
+    project = ('ICC 101/')
+    if not os.path.isdir(root + project):
+        os.mkdir(root + project)
+    batch = directory[-5:]
+    if not os.path.isdir(root + project + batch):
+        os.mkdir(root + project + batch)
+    labels = '/labels.csv'
+    return root + project + batch + labels
+
+def makeDF(directory):
+    with open(directory + '/labels.csv', 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        labels = ['path', 'result']
+        csvwriter.writerow(labels)
+        for filename in os.listdir(directory):
+            if filename.endswith('_0.jpg'):
+                csvwriter.writerow([filename ,'0'])
+            elif filename.endswith('.jpg'):
+                csvwriter.writerow([filename, '-1'])
+
+
+def directoryIterator(directory):
+    labelsdf = pd.read_csv(directory + '/labels.csv')
+    numrows = labelsdf.count()['path']
+    history = []
+    row = 0
+    while row < numrows:
+        if labelsdf['result'].at[row] == -1:
+            filePath = directory +'/' + labelsdf['path'].at[row]
+            data = easygui.enterbox(msg=prompt, default=1, image=filePath)
+            if data in mark_dictionary:
+                if data == 'qw/os':
+                    return
+                elif data == 'u':
+                    if len(history) != 0:
+                        row = history.pop()
+                        labelsdf['result'].at[row] = -1
+                elif data == '1u':
+                    if len(history) != 0:
+                        row = history.pop()
+                        labelsdf['result'].at[row] = -1
+                elif data == 'p':
+                    string = 'Bubbles Labeled: ' +  str(len(history)) + '\n'
+                    for i in history:
+                        string += labelsdf['path'].at[i] + ': ' + str(labelsdf['result'].at[i]) + '\n'
+                    easygui.msgbox(msg=string)
+                elif data == 's':
+                    labelsdf.to_csv(directory + '/labels.csv', index=False)
+                    shutil.copy(directory + '/labels.csv', get_label_folder_path(directory))
+                    history = []
+                elif data == 'q':
+                    labelsdf.to_csv(directory + '/labels.csv', index=False)
+                    shutil.copy(directory + '/labels.csv', get_label_folder_path(directory))
+                    return
+                else:
+                    labelsdf['result'].at[row] = data
+                    history.append(row)
+                    row += 1
+        else:
+            row += 1
+    if easygui.ynbox(msg='Save?'):
+        labelsdf.to_csv(directory + '/labels.csv', index=False)
+        shutil.copy(directory + '/labels.csv', get_label_folder_path(directory))
+directory = sys.argv[1]
+if not os.path.exists(directory + '/labels.csv'):
+    makeDF(directory)
+directoryIterator(directory)
+
